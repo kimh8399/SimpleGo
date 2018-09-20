555e1e1b4d5113a8c4e813479475e0abe8e99e81
kimh8399 <kimh8399@googleapps.wrdsb.ca>
2018-09-20 17:13:07 -0400

    Changed some of the game logic

--- app/src/main/java/com/example/huck/simplego/structures/Board.java
+++ app/src/main/java/com/example/huck/simplego/structures/Board.java
@@ -19,13 +19,13 @@ class Coord{
 
 class Stone{
     Color color;                        // Color of the stone
-    Coord [] connected;                 // Other stones connected to it
+    Vector <Coord> connected;                 // Other stones connected to it
     int liberties;                      // Initial liberties
 
     Stone(){
         color = Color.Empty;            // Initializes the color of the stone to empty
         liberties = 4;                  // Each stone starts with 4 liberties
-        connected = new Coord [4];      // Creates the list of connected stones
+        connected = new Vector<>();      // Creates the list of connected stones
     }
 }
 
@@ -46,17 +46,17 @@ public class Board {
     }
 
     /*Returns the cardinal locations for certain stones (friendly, enemy, or both) */
-    public Vector<Coord> get_cardinal(Coord loc, StoneType type){
+    public Vector<Coord> get_cardinal(Stone [][] buffer, Coord loc, StoneType type){
 
         Color checkColor;
         Vector<Coord> cardinal = new Vector<>();
 
         switch (type) {
             case Friendly:
-                checkColor = boardState[loc.x][loc.y].color;
+                checkColor = buffer[loc.x][loc.y].color;
                 break;
             case Enemy:
-                checkColor = (boardState[loc.x][loc.y].color == Color.Black) ? Color.White : Color.Black;
+                checkColor = (buffer[loc.x][loc.y].color == Color.Black) ? Color.White : Color.Black;
                 break;
             case Both:
                 checkColor = Color.Empty;
@@ -65,31 +65,46 @@ public class Board {
                 checkColor = Color.Empty;
                 break;
         }
-        if(loc.x + 1 < BOARD_SIZE && ((boardState[loc.x + 1][loc.y].color == checkColor) || checkColor == Color.Empty)){
+        if(loc.x + 1 < BOARD_SIZE && ((buffer[loc.x + 1][loc.y].color == checkColor) || checkColor == Color.Empty)){
             cardinal.add(new Coord(loc.x + 1, loc.y));
         }
-        if(loc.x - 1 > 0 && ((boardState[loc.x - 1][loc.y].color == checkColor) || checkColor == Color.Empty)){
+        if(loc.x - 1 > 0 && ((buffer[loc.x - 1][loc.y].color == checkColor) || checkColor == Color.Empty)){
             cardinal.add(new Coord(loc.x - 1, loc.y));
         }
-        if(loc.y + 1 < BOARD_SIZE && ((boardState[loc.x][loc.y+ 1].color == checkColor) || checkColor == Color.Empty)) {
+        if(loc.y + 1 < BOARD_SIZE && ((buffer[loc.x][loc.y+ 1].color == checkColor) || checkColor == Color.Empty)) {
             cardinal.add(new Coord(loc.x, loc.y + 1));
         }
-        if(loc.y - 1 > 0 && ((boardState[loc.x][loc.y - 1].color == checkColor) || checkColor == Color.Empty)) {
+        if(loc.y - 1 > 0 && ((buffer[loc.x][loc.y - 1].color == checkColor) || checkColor == Color.Empty)) {
             cardinal.add(new Coord(loc.x, loc.y - 1));
         }
 
         return cardinal;
     }
 
-    public void connect_group(){
-
+    public void connect_group(Stone [][] buffer, Coord loc){
+        Vector<Coord> cardinal = get_cardinal(buffer, loc, StoneType.Friendly);
+        for(int i = 0; i < cardinal.size(); i++){
+            buffer[cardinal.get(i).x][cardinal.get(i).y].connected.add(loc);
+            buffer[cardinal.get(i).x][cardinal.get(i).y].liberties--;
+            buffer[loc.x][loc.y].connected.add(cardinal.get(i));
+            buffer[loc.x][loc.y].liberties--;
+        }
     }
 
-    public boolean has_liberties(Coord loc){
-        int liberties = 0;
 
 
-        return true;
+    public int has_liberties(Coord loc){
+
+        Vector <Coord> visited = new Vector<>();
+        Vector <Coord> cardinal;
+        cardinal = get_cardinal(boardState, loc, StoneType.Friendly);
+
+        for(int i = 0; i < cardinal.size(); i++){
+            visited.add(cardinal.get(i));
+            has_liberties(cardinal.get(i));
+        }
+
+        return 0;
     }
 
     public void remove_stone(Coord loc){
@@ -102,5 +117,9 @@ public class Board {
 
     public static void start_game(){
 
+    }
+
+    public static void main(String args[]){
+        Board board = new Board();
     }
 }
